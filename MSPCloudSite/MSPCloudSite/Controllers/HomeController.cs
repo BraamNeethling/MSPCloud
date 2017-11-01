99aeaf9e8bca53f6677f0b338581b79afea5c1df
DESKTOP-FK50D1P\braam <braamneethling1991@gmail.com>
2017-11-01 19:25:03 +0200

    wip : adding all proj files

---
+++ MSPCloudSite/MSPCloudSite/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MSPCloudSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        public ActionResult MSPCloudServices()
+        {
+            return View();
+        }
+        public ActionResult PartnerServices()
+        {
+            return View();
+        }
+        public ActionResult Careers()
+        {
+            return View();
+        }
+    }
+}
