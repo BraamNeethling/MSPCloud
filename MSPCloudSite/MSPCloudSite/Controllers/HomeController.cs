3a0214736d302df4d356261980b1f4bca652885a
DESKTOP-FK50D1P\braam <braamneethling1991@gmail.com>
2017-11-03 07:49:03 +0200

    wip : updated mailer and added new contact numbers field

--- MSPCloudSite/MSPCloudSite/Controllers/HomeController.cs
+++ MSPCloudSite/MSPCloudSite/Controllers/HomeController.cs
@@ -42,17 +42,15 @@ namespace MSPCloudSite.Controllers
         private static MailMessage SetMailMessage(MailModel model)
         {
 
-            const string body = "<p>Email From: {0} ({1})</p><p>Message:</p><p>{2}</p>";
+            const string body = "<p>Email From: {0} ({1})</p><p>Message:</p><p>{2}</p><p>{3}</p>";
             var message = new MailMessage();
             message.To.Add(new MailAddress("braamneethling1991@gmail.com"));
             message.From = new MailAddress("braamneethling1991@gmail.com");
-            message.Subject = "Client Interest";
-            message.Body = string.Format(body, model.FromName, model.FromEmail, model.Message);
+            message.Subject = "Client Request" + " from " + model.FromName;
+            message.Body = string.Format(body, model.FromName, model.FromEmail, model.Message, model.ContactNumber);
             message.IsBodyHtml = true;
 
             var htmlView = SetEmailLogo(model);
-
-
             message.AlternateViews.Add(htmlView);
 
             return message;
@@ -69,10 +67,13 @@ namespace MSPCloudSite.Controllers
                 };
 
             var htmlView = AlternateView.CreateAlternateViewFromString(
+                "<div>" +
                 "<img src=cid:Logo><br/> " +
-                "FromName :" + model.FromName + "<br/>" +
-                "FromEmail :" + model.FromEmail + "<br/>" +
-                "Message :" + model.Message + "<br/>",
+                "From Name :" + model.FromName + "<br/>" +
+                "From Email :" + model.FromEmail + "<br/>" +
+                "Message :" + model.Message + "<br/>" +
+                "Contact Number :" + model.ContactNumber + "<br/>" +
+                "</div>",
                 null, "text/html");
 
             htmlView.LinkedResources.Add(linkedImage);
--- MSPCloudSite/MSPCloudSite/js/MSPCloudServices/MSPCloudServices.js
+++ MSPCloudSite/MSPCloudSite/js/MSPCloudServices/MSPCloudServices.js
@@ -1,1 +1,8 @@
-﻿
+﻿var MSPCloudSite = MSPCloudSite || {};
+MSPCloudSite.js = MSPCloud.js || {};
+MSPCloudSite.js.MSPCloudServices = MSPCloudSite.js.MSPCloudServices || {};
+
+(function(ns) {
+    
+
+})(MSPCloud.js.MSPCloudServices)
